c5d1659c786ae0552eaff46ba34e88703fb4f70d
Kemal can bayramoğlu <bayramoglukemalcan@gmail.com>
2026-02-18 01:30:20 +0300

    lucide icon added and spinning

--- src/components/test/navbar/page.tsx
+++ src/components/test/navbar/page.tsx
@@ -1,28 +1,33 @@
+"use client";
+import { useState, } from "react";
+import { LucideMenu, X } from "lucide-react";
 import Image from "next/image";
 import { NAVBAR_PAGES } from "./constants";
 import Link from "next/link";
 export default function Navbar() {
 
+    const [IsOpen, SetIsOpen] = useState(false);
+    const toggleMenu = () => SetIsOpen((prev) => !prev);
     return (
         <main >
             <nav className="w-full h-20 flex items-center gap-8 px-10 bg-white fixed">
-                <Link className="flex gap-10 " href={"/Home"}>
+                <Link className="flex items-center gap-10 " href={"/Home"}>
                     <Image src={"/tree.png"} width={40} height={40} alt={"Tree"} />
                     <h1 className="text-2xl font-bold text-green-900 tracking-tighter italic">CalmaTierra</h1>
                 </Link>
                 <div className=" md:flex hidden items-center ml-auto gap-8">
                     {NAVBAR_PAGES.map((item, index) => (
-                        <Link className=" text-green-900 " key={index} href={item.href}> {item.page} </Link>
+                        <Link className=" text-green-900 font-semibold" key={index} href={item.href}> {item.page} </Link>
                     ))}
                     <Link href={"/ContactUs"}>
-                        <button className="px-4 hover:bg-green-800 py-2 rounded-md bg-green-900">  Contact Us  </button>
+                        <button className="px-4 hover:bg-green-800 font-semibold py-2 rounded-md bg-green-900">  Contact Us  </button>
                     </Link>
                 </div>
+                <button onClick={toggleMenu} className={`md:hidden ml-auto duration-200 text-green-900 ${IsOpen ? "rotate-0" : "rotate-90"}`}>
 
-
-
-
-
+                    {IsOpen ? < LucideMenu /> : < X className="rotate:45" />}
+                    {/* <LucideMenu className="md:hidden ml-auto text-green-900 flex "></LucideMenu> */}
+                </button>
             </nav>
         </main >
     )
