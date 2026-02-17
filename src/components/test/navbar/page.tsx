eb7a007e6119bdaaa6653c8a3e46b00f522c5879
Kemal can bayramoğlu <bayramoglukemalcan@gmail.com>
2026-02-18 02:08:25 +0300

    i think prev one better but sleep

--- src/components/test/navbar/page.tsx
+++ src/components/test/navbar/page.tsx
@@ -17,17 +17,35 @@ export default function Navbar() {
                 </Link>
                 <div className=" md:flex hidden items-center ml-auto gap-8">
                     {NAVBAR_PAGES.map((item, index) => (
-                        <Link className=" text-green-900 font-semibold" key={index} href={item.href}> {item.page} </Link>
+                        <Link className=" text-green-900 font-semibold" key={index} href={item.href}>
+                            {item.page}
+                        </Link>
                     ))}
                     <Link href={"/ContactUs"}>
                         <button className="px-4 hover:bg-green-800 font-semibold py-2 rounded-md bg-green-900">  Contact Us  </button>
                     </Link>
                 </div>
-                <button onClick={toggleMenu} className={`md:hidden ml-auto duration-200 text-green-900 ${IsOpen ? "rotate-0" : "rotate-90"}`}>
 
+                <button onClick={toggleMenu} className={`md:hidden ml-auto duration-200 relative text-green-900 ${IsOpen ? "rotate-0" : "rotate-90"}`}>
                     {IsOpen ? < LucideMenu /> : < X className="rotate:45" />}
-                    {/* <LucideMenu className="md:hidden ml-auto text-green-900 flex "></LucideMenu> */}
                 </button>
+
+                <div className="relative md:hidden">
+                    <div className="bg-amber-400">
+                        {NAVBAR_PAGES.map((item, index) => (
+
+                            IsOpen &&
+                            <div className="text-bold text-xl px-6 py-6 bg-white h-screen w-screen  flex flex-col gap-4">
+
+                                <Link className=" text-green-900 font-semibold" onClick={() => SetIsOpen(false)} key={index} href={item.href}>
+                                    {item.page}
+                                </Link>
+                            </div>
+                        ))}
+
+                    </div>
+
+                </div>
             </nav>
         </main >
     )
