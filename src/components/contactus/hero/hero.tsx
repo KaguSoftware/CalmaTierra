54873f0435e581b44c05a2eaaee3b5346eea4018
Kemal can bayramoğlu <bayramoglukemalcan@gmail.com>
2026-02-19 00:56:54 +0300

    pulling mains updates

--- src/components/contactus/hero/hero.tsx
+++ src/components/contactus/hero/hero.tsx
@@ -2,7 +2,8 @@ import Image from "next/image"
 
 export default function HeroContactUs() {
     return (
-        <div className="w-full h-40 bg-green-500">
+        <div className="w-full h-80  bg-green-700">
+            <p className="absolute text-4xl top-30 text-white font-semibold">Get in Touch with CalmaTierra</p>
 
         </div>
 
