d772e0004c243d12623d95c1bb6ca9ce963b5058
Parsa <parsaxavier@gmail.com>
2026-02-20 10:54:20 +0300

    footer details

--- src/components/Footer/constants.ts
+++ src/components/Footer/constants.ts
@@ -2,40 +2,40 @@ import { Mail, MapPin, Phone } from "lucide-react";
 import type { FooterBrand, FooterContactItem, FooterContents } from "./types";
 
 export const FOOTER_BRAND: FooterBrand = {
-    name: "Footer.name",
-    description: "Footer.description",
-    image: {
-        src: "/CalmaTierraLogo.jpeg",
-        alt: "CalmaTierra Logo"
-    },
+	name: "Footer.name",
+	description: "Footer.description",
+	image: {
+		src: "/calmatierra icon.png",
+		alt: "CalmaTierra Logo",
+	},
 };
 
 export const FOOTER_CONTACTS: FooterContactItem[] = [
-    {
-        label: "Footer.location",
-        Icon: MapPin
-    },
-    {
-        label: "Footer.email",
-        Icon: Mail
-    },
-    {
-        label: "Footer.phone",
-        Icon: Phone
-    },
+	{
+		label: "Footer.location",
+		Icon: MapPin,
+	},
+	{
+		label: "Footer.email",
+		Icon: Mail,
+	},
+	{
+		label: "Footer.phone",
+		Icon: Phone,
+	},
 ];
 
 export const FOOTER_CONTENTS: FooterContents = {
-    year: "Footer.year",
-    bottomText: "Footer.text",
-    bottomLinks: [
-        {
-            label: "Footer.link.labelOne",
-            href: "/privacy"
-        },
-        {
-            label: "Footer.link.labelTwo",
-            href: "/terms"
-        },
-    ],
+	year: "Footer.year",
+	bottomText: "Footer.text",
+	bottomLinks: [
+		{
+			label: "Footer.link.labelOne",
+			href: "/privacy",
+		},
+		{
+			label: "Footer.link.labelTwo",
+			href: "/terms",
+		},
+	],
 };
