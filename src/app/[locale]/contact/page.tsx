6c27bd21f216d72c43079b6cea524bb379c424a0
Parsa <parsaxavier@gmail.com>
2026-02-20 23:42:40 +0300

    removed map

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,14 +1,12 @@
 import Card from "@/src/components/contactus/card/Card";
-import Maplocation from "@/src/components/contactus/card/location/location";
 import Contacts from "@/src/components/contactus/contacts/contacts";
 import HeroContactUs from "@/src/components/contactus/hero/hero";
 
 export default function ContactPage() {
-    return (
-        <main className="grid gap-10 mt-5">
-            <HeroContactUs />
-            <Card />
-            <Maplocation />
-        </main>
-    );
+	return (
+		<main className="grid gap-10 mt-5">
+			<HeroContactUs />
+			<Card />
+		</main>
+	);
 }
